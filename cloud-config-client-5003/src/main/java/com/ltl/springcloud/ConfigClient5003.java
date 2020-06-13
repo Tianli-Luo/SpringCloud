3f472bde4ab48f230ad94d26b77955973a30c1d3
Tianli_Luo <2401782562@qq.com>
2020-06-13 11:24:09 +0800

    使用了SpringCloudBus 消息总线

---
+++ cloud-config-client-5003/src/main/java/com/ltl/springcloud/ConfigClient5003.java
@@ -0,0 +1,17 @@
+package com.ltl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ltl
+ * @Classname ConfigClient5002
+ * @Description TODO
+ * @Date 2020/6/12 22:16
+ */
+@SpringBootApplication
+public class ConfigClient5003 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient5003.class,args);
+    }
+}
